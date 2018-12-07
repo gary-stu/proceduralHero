7789771f84ebfaa84f301dd657fe840be5dc63b4
gary-stu <45605994+gary-stu@users.noreply.github.com>
2018-12-07 06:12:21 +0100

    latest version of pH

--- pH.py
+++ pH.py
@@ -3,53 +3,62 @@ from os import walk, path, system, popen
 from random import randint, choice
 from subprocess import Popen
 from time import sleep
+import platform
 
 class pH:
 	# Initialize the settings
 	def __init__(self):
-		self.rounds_path = r''
-		self.intervals_path = r''
-
-		self.custom_finish = True
-		self.finishes_path = r''
+		# User defined settings
+		# Pathes start with a r: r'C:\Users\Anon', or r'/home/Anon'
+		# Pathes can be absolute pathes or relative Pathes (from where the python file is located)
+		self.rounds_path = r'rounds'
+		self.intervals_path = r'intervals'
+		self.finishes_path = r'finishes'
 		
+		# use_* are boolean
+		# must be True or False
+		self.use_intervals = True
+		self.use_custom_finish = True
+		
+		# The number of rounds will be randomized between those two values
+		# Note: You'll need at least as many round videos (and intervals if you use them) as 'self.max_rounds'
+		# If you want a given number of rounds, just set min and max to same value
 		self.min_rounds = 6
 		self.max_rounds = 16
 		
+		# The extensions of the file you're looking for
+		# You probably won't ever have to change this, unless you use obscure file formats
 		self.extensions = ['webm', 'mkv', 'mp4', 'avi', 'mov', 'wmv', 'mpg', 'mpeg']
 		
-		self.log = "pH_log_0.txt"
+		# End of user defined settings
+		self.playlist = "pH_playlist_0.txt"
 		nb = 0
-		while path.isfile(self.log):
+		while path.isfile(self.playlist):
 			nb += 1
-			self.log = 'pH_log_' + str(nb) + '.txt'
-		self.playlist = 'pH_playlist_' + str(nb) + '.txt'
-		system('rm ' + self.playlist)
-		
+			self.playlist = 'pH_playlist_' + str(nb) + '.txt'
+		self.log = 'pH_log_' + str(nb) + '.txt'
 		self.rounds = []
 		self.intervals = []
 		self.finishes = []
+		self.messages = []
+		self.mpv_installed = True
 
 
-	# Print str both to screen and in the logfile
+	# Print str to screen and put it in the message list for possible output to logfile
 	def info(self, str):
 		print(str)
-		log = open(self.log, 'a')
-		log.write(str + '\n')
-		log.close()
+		self.messages.append(str)
 
-
-	# Search folder recursively for files with correct extensions in folder dir
+	# Search folder recursively for files with correct extensions in folder 'dir'
 	# Append the results to list
 	def files(self, dir, list):
 		for root, directories, filenames in walk(dir):
 			for filename in filenames:
-				if filename.split('.')[-1] in self.extensions : 
-					list.append(path.join(root, filename))
+				if filename.split('.')[-1] in self.extensions : list.append(path.join(root, filename))
 
 
 	# Test the config
-	# If it returns True, pH CAN run
+	# If it returns True, pH SHOULD run without problem
 	# Else, error is described in logfile and terminal
 	def test_conf(self):
 		result = True
@@ -64,18 +73,23 @@ class pH:
 			self.info('...done')
 		self.info('')
 			
-		self.info('Testing "self.intervals_path"')
-		if not(path.isdir(self.intervals_path)):
-			result = False
-			self.info('Error')
-			self.info('    "self.intervals_path" is not a valid directory')
-			self.info('    current: "' + self.intervals_path + '"')
+		if self.use_intervals:
+			self.info('"self.intervals" is set to True')
+			self.info('Testing "self.intervals_path"')
+			if not(path.isdir(self.intervals_path)):
+				result = False
+				self.info('Error')
+				self.info('    "self.intervals_path" is not a valid directory')
+				self.info('    current: "' + self.intervals_path + '"')
+			else:
+				self.info('...done')
+			self.info('')
 		else:
-			self.info('...done')
-		self.info('')
+			self.info('"self.use_intervals" is set to False')
+			self.info('Skipping all intervals related tests')
 			
-		if self.custom_finish:
-			self.info('"self.custom_finish" is set to True')
+		if self.use_custom_finish:
+			self.info('"self.use_custom_finish" is set to True')
 			self.info('Testing "self.finishes_path"')
 			if not(path.isdir(self.finishes_path)):
 				result = False
@@ -86,7 +100,7 @@ class pH:
 				self.info('...done')
 			self.info('')
 		else:
-			self.info('"self.custom_finish" is set to False')
+			self.info('"self.use_custom_finish" is set to False')
 			self.info('Skipping all finishes related tests')
 			self.info('')
 				
@@ -102,19 +116,19 @@ class pH:
 				self.info('...done')
 			self.info('')
 			
+			if self.use_intervals:
+				self.info('Trying to find interval files in "' + self.intervals_path + '"')
+				self.files(self.intervals_path, self.intervals)
+				if len(self.intervals) < (self.max_rounds - 1):
+					result = False
+					self.info('Error')
+					self.info('    ' + str(len(self.intervals)) + ' files found in "' + self.intervals_path + '"')
+					self.info('    minimum number required, based on (self.max_rounds - 1) is ' + str(self.max_rounds - 1))
+				else:
+					self.info('...done')
+				self.info('')
 			
-			self.info('Trying to find interval files in "' + self.intervals_path + '"')
-			self.files(self.intervals_path, self.intervals)
-			if len(self.intervals) < (self.max_rounds - 1):
-				result = False
-				self.info('Error')
-				self.info('    ' + str(len(self.intervals)) + ' files found in "' + self.intervals_path + '"')
-				self.info('    minimum number required, based on (self.max_rounds - 1) is ' + str(self.max_rounds - 1))
-			else:
-				self.info('...done')
-			self.info('')
-			
-			if self.custom_finish:
+			if self.use_custom_finish:
 				self.info('Trying to find finishes files in "' + self.finishes_path + '"')
 				self.files(self.finishes_path, self.finishes)
 				if len(self.finishes) == 0:
@@ -130,11 +144,14 @@ class pH:
 			self.info('Checking if mpv can be used from terminal')
 			test = popen('mpv --version').read()
 			if test.startswith('mpv'):
+				self.mpv_installed = True
 				self.info('...done')
 			else:
-				result = False
-				self.info('Error')
+				self.mpv_installed = False
+				#result = False
+				#self.info('Error')
 				self.info('    mpv could not be used from terminal')
+				#self.info('    Please either install it if not done already and/or add it to your PATH')
 			self.info('')
 		return result
 
@@ -148,16 +165,18 @@ class pH:
 		while i < (nb_rounds - 1):
 			round = choice(self.rounds)
 			self.rounds.remove(round)
-			interval = choice(self.intervals)
-			self.intervals.remove(interval)
 			self.info('Round ' + str(i + 1) + '   : ' + round)
-			self.info('Interval ' + str(i + 1) + ': ' + interval)
 			pl.write(round + '\n')
-			pl.write(interval + '\n')
+			
+			if self.use_intervals:
+				interval = choice(self.intervals)
+				self.intervals.remove(interval)
+				self.info('Interval ' + str(i + 1) + ': ' + interval)
+				pl.write(interval + '\n')
 			self.info('')
 			i += 1
 		round = ""
-		if self.custom_finish:
+		if self.use_custom_finish:
 			round = choice(self.finishes)
 		else:
 			round = choice(self.rounds)
@@ -174,13 +193,34 @@ class pH:
 		self.info('Number of rounds : ' + str(nb_rounds))
 
 
-	# play the generated playlist
+	# Play the generated playlist
+	# Try to use a static binary if mpv was not found on system
+	# Win binary comes from https://mpv.srsfckn.biz/
+	# OSX binary comes from https://laboratory.stolendata.net/~djinn/mpv_osx/
+	# Haven't found a standalone binary for linux
 	def play(self):
+		mpv = 'mpv'
+		if not self.mpv_installed:
+			myOs = platform.system()
+			if myOs == 'Windows':
+				mpv = 'exes/mpv_win.exe'
+			elif myOs == 'Darwin':
+				mpv = 'exes/mpv_osx'
+			elif myOs == 'Linux':
+				self.info('No static binary available for Linux')
+				self.info('Please install mpv through your package manager')
+				self.info('or through mpv-build') 
+				self.info('')
+				self.info('Error detected, outputting to logfile')
+				log = open(self.log, 'a')
+				for n in self.messages:
+					log.write(n + '\n')
+				log.close()
+				return()
+	
 		print('Starting in 5 seconds, prepare yourself!')
 		sleep(5)
-		Popen('mpv -fs --playlist="' + self.playlist + '"')
-		sleep(1)
-		system('rm ' + self.playlist)
+		Popen(mpv + ' -fs --playlist="' + self.playlist + '"', shell=True)
 
 
 	# main
@@ -191,7 +231,11 @@ class pH:
 			self.procedural_Hero()
 			self.play()
 		else:
-			self.info('Stop')
+			self.info('Error detected, outputting to logfile')
+			log = open(self.log, 'a')
+			for n in self.messages:
+				log.write(n + '\n')
+			log.close()
 
-hero = pH()
-hero.start()
+acid = pH()
+acid.start()
